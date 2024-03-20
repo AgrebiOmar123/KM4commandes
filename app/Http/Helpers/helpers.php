3aebfcd6e2db9dbabc70b4b155c01a4fa45fe00c
AgrebiOmar123 <oagrebi614@gmail.com>
2024-03-20 14:15:03 +0100

    55

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -19,11 +19,11 @@ function distinguer ($nombre) {
     } elseif ($nombre < 100) { // 20 - 99
         $valeur = distinguer($nombre / 10) . ' dix' . distinguer($nombre % 10);
     } elseif ($nombre < 200) { // 100 - 199
-        $valeur = ' seratus' . distinguer($nombre -100);
+        $valeur = ' cent' . distinguer($nombre -100);
     } elseif ($nombre < 1000) { // 200 - 999
         $valeur = distinguer($nombre / 100) . ' cent' . distinguer($nombre % 100);
     } elseif ($nombre < 2000) { // 1.000 - 1.999
-        $valeur = ' seribu' . distinguer($nombre -1000);
+        $valeur = ' un millier' . distinguer($nombre -1000);
     } elseif ($nombre < 1000000) { // 2.000 - 999.999
         $valeur = distinguer($nombre / 1000) . ' mille' . distinguer($nombre % 1000);
     } elseif ($nombre < 1000000000) { // 1000000 - 999.999.990
@@ -31,4 +31,40 @@ function distinguer ($nombre) {
     }
 
     return $valeur;
+}
+
+/*************************** */
+
+function format_date($tgl, $jour = true)
+{
+    $nom_jour  = array(
+        'Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'
+              
+    );
+    $nom_mois = array(1 =>
+        'Janvier', 'Fevrier', 'Mars', 'Avril', 'Mai', 'Juin', 'Julliet', 'Aout', 'September', 'October', 'November', 'December'
+    );
+
+    $annee   = substr($tgl, 0, 4);
+    $mois   = $nom_mois[(int) substr($tgl, 5, 2)];
+    $date = substr($tgl, 8, 2);
+    $text    = '';
+
+    if ($jour) {
+        $ordre_jour = date('w', mktime(0,0,0, substr($tgl, 5, 2), $date, $annee));
+        $jour        = $nom_jour[$ordre_jour];
+        $text       .= "$jour, $date $mois $annee";
+    } else {
+        $text       .= "$date $mois $annee";
+    }
+    
+    return $text; 
+}
+
+
+/************************** */
+
+function ajouter_zero_enavant($value, $seuil = null)
+{
+    return sprintf("%0". $seuil . "s", $value);
 }
